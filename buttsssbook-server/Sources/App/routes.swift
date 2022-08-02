8bd8f703976e5a228910a381b696b4152fd30bf4
Mateus Rodrigues <mateus.fr26@gmail.com>
2022-08-02 16:51:04 -0300

    Add route to display all routes

--- buttsssbook-server/Sources/App/routes.swift
+++ buttsssbook-server/Sources/App/routes.swift
@@ -2,19 +2,18 @@ import Vapor
 
 func routes(_ app: Application) throws {
     
-    try app.register(collection: UserController())
-    try app.register(collection: PostController())
-    try app.register(collection: ChatController())
-    try app.register(collection: LikeController())
+    app.routes.get { req -> String in
+        let routes = req.application.routes.all
+        return routes.map(\.description).joined(separator: "\n")
+    }
     
     app.routes.get("documentation") { req in
         req.view.render(app.directory.publicDirectory + "documentation/index.html")
     }
     
-    app.routes.post("foo") { req -> Response in
-        let uuid = try req.content.decode(UUID.self)
-        print("FOO: \(uuid)")
-        return Response(status: .noContent)
-    }
+    try app.register(collection: UserController())
+    try app.register(collection: PostController())
+    try app.register(collection: ChatController())
+    try app.register(collection: LikeController())
     
 }
